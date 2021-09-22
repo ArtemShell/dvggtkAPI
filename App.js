b533ade12c4fbfc5ef2f2d097cbd20952041a92e
Artemy Shelep <tema@shelep.ml>
2021-09-22 16:03:47 +1000

    fix

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ const express = require('express');
 const routes = require('./routes/routes');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 
 app.use(express.json());
 routes(app);
